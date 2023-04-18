72b1bf721a0c75c0562573297f30d9eed3e38b39
DibyaRanjan1 <dibyaranjantripathy1@gmail.com>
2023-04-18 20:54:41 +0530

    Graph code with BFS added for disconnected graph.

--- src/Graph/MyGraph.java
+++ src/Graph/MyGraph.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 public class MyGraph {
     public static void main(String[] args) {
 
-        int v=5;
+        int v=6;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(v);
 
         for(int i=0;i< v;i++){
@@ -18,8 +18,13 @@ public class MyGraph {
         AddEdge(adj,0,2);
         AddEdge(adj,1,3);
         AddEdge(adj,1,2);
+        AddEdge(adj,4,5);
 
-        PrintGraph(adj);
+       // PrintGraph(adj);
+
+       // BFSUndirectedGraph(adj,4,0);
+
+        BFSDisconnectedGraph(adj,6,0);
     }
 
     public static void AddEdge(ArrayList<ArrayList<Integer>> adj,int u,int v){
@@ -37,11 +42,15 @@ public class MyGraph {
         }
     }
 
-    public void BFSUndirectedGraph(ArrayList<ArrayList<Integer>> adjList,int v, int s){
-        Boolean[] visited = new Boolean[v+1];
+    public static void BFSUndirectedGraph(ArrayList<ArrayList<Integer>> adjList,int v, int s){
+        Boolean visited[] = new Boolean[v+1];
+
+        for(int i=0;i<visited.length;i++){
+            visited[i] = false;
+        }
 
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(adjList.get(s).get(s));
+        queue.add(s);
         visited[s] = true;
 
         while(!queue.isEmpty()){
@@ -60,5 +69,42 @@ public class MyGraph {
         }
     }
 
+    public static void BFSDisconnectedGraph(ArrayList<ArrayList<Integer>> adjList,int v, int s){
+        Boolean visited[] = new Boolean[v+1];
+
+        for(int i=0;i<visited.length;i++){
+            visited[i] = false;
+        }
+
+        for (int i=0;i<adjList.size();i++){
+            if(!visited[i]){
+                BFSGraph(adjList,i,visited);
+            }
+
+        }
+
+
+    }
+
+    public static void BFSGraph(ArrayList<ArrayList<Integer>> adjList, int s,Boolean[] visited){
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        visited[s] = true;
+
+        while(!queue.isEmpty()){
+            Integer data =   queue.poll();
+            System.out.println(data + " ");
+
+            for(int u: adjList.get(data)){
+
+                if(!visited[u]){
+                    visited[u] = true;
+                    queue.add(u);
+                }
+            }
+        }
+    }
 
 }
