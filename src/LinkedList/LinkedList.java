eeabf590935a3bc11cd75f20c04f5a8866f7fbd5
DibyaRanjan1 <dibyaranjantripathy1@gmail.com>
2023-06-19 21:43:53 +0530

    Detect loop, reverse a linkedlist problem added.

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -4,6 +4,14 @@ public class LinkedList {
 
 
     public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+      Node newLinkedList =   ReverseLinkedList(head);
+
 
     }
 
@@ -20,6 +28,24 @@ public class LinkedList {
             if(slow ==fast) { return true;}
         }
         return false;
+    }
+
+    public static Node ReverseLinkedList(Node head){
+
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        while(curr !=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+
+        return head;
     }
 
 
